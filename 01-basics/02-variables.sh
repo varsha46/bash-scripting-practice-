2ad7ebab8be493c51b3849402ae30290ccaf404b
EC2 Default User <ec2-user@ip-172-31-6-134.ap-south-1.compute.internal>
2026-08-13 10:23:12 +0000

    Add Bash practice

---
+++ 01-basics/02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+##variable
+
+name='varsha' ##assigning a variable,where variable is name!
+echo my name is $name ## $ tells bash : Don't treat name as ordinary text. Look inside the variable called name and give me its value.
